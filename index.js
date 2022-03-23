bc5052c7587c84c894b7c7190d31ecdb66f9e08c
Teewai57 <bobtoyoabasi@gmail.com>
2022-03-23 15:13:15 +0100

    Fetched API

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Fetch API
+
+const getTodo = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await res.json()
+    console.log(data);
+}
