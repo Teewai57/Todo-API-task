e3ed6b30ea18923f4f5367808a5598eec8464c39
Teewai57 <bobtoyoabasi@gmail.com>
2022-03-23 20:28:33 +0100

    changed texts' color

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const getTodo = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
     const data = await res.json()
     console.log(data);
+
+    let todoList = data.map((post) => {
+        return `
+        <h6>${post.title}</h6>
+        
+        `
+    })
+    document.querySelector('.container').innerHTML = todoList;
 }
 
 getTodo()
